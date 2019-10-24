7d5c2d0581387df60eb5b0ad53f0028bdc557e8d
shubham m gohel <smg56@afsconnect1.njit.edu>
2019-10-24 14:05:53 -0400

    updated file

--- Project/registration.php
+++ Project/registration.php
@@ -1,7 +1,43 @@
 <!DOCTYPE html>
+<?php
+function checkPasswords(){
+	if(isset($_POST['password']) && isset($_POST['confirm'])){
+		if($_POST['password'] == $_POST['confirm']){
+			echo "<br>Passwords Matched!<br>";
+		}
+		else{
+			echo "<br>Password Didn't match<br>";
+			return true;
+		}
+	}
+}
+?>
 <html>
 <meta charset="utf-8">
 <link rel="stylesheet" href="style.css" />
+<head>
+<script>
+function validate(){
+	var form = document.forms[0];
+	var password = form.password.value;
+	var conf = form.confirm.value;
+	console.log(password);
+	console.log(conf);
+	let pv = document.getElementById("validation.password");
+	let succeeded = true;
+	if(password == conf){
+		
+		pv.style.display = "none";
+	
+	}
+	else{
+		pv.style.display = "block";
+		pv.innerText = "Passwords don't match";
+		
+		succeeded = false;
+	}
+}
+</script>
 </head>
 <body>
 <?php
@@ -17,15 +53,26 @@ if (isset($_REQUEST['username'])){
 	$password = stripslashes($_REQUEST['password']);
 	$password = mysqli_real_escape_string($con,$password);
 	$trn_date = date("Y-m-d H:i:s");
-        $query = "INSERT into `users1` (username, password, email, trn_date)
-VALUES ('$username', '".md5($password)."', '$email', '$trn_date')";
+       
+	if(checkPasswords() === true ){
+	echo "<div class='form'>
+	<h3>You are not registered successfully.</h3>
+	<br/>Click here to <a href='login.php'>Login</a></div>";
+	}
+
+        else{
+	    $query = "INSERT into `users1` (username, password, email, trn_date)
+        VALUES ('$username', '".md5($password)."', '$email', '$trn_date')";
         $result = mysqli_query($con,$query);
-        if($result){
             echo "<div class='form'>
 <h3>You are registered successfully.</h3>
-<br/>Click here to <a href='login.php'>Login</a></div>";
+<br/>Click here to <a href='login.php'>Login</a>
+<br/>
+<br/>Enter your information below  to register
+</div>";
+
         }
-    }else{
+    }
 ?>
 <div class="form">
 <h1>Registration</h1>
@@ -33,9 +80,11 @@ VALUES ('$username', '".md5($password)."', '$email', '$trn_date')";
 <input type="text" name="username" placeholder="Username" required />
 <input type="email" name="email" placeholder="Email" required />
 <input type="password" name="password" placeholder="Password" required />
+<input type="password" name="confirm" placeholder="Re-Enter password"/>
+<span style="display:none;" id="validation.password"></span>
 <input type="submit" name="submit" value="Register" />
 </form>
 </div>
-<?php } ?>
+<?php ?>
 </body>
 </html>
